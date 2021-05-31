c5c0ed4bc841b5b8003094b570379e14fbf81ce1
Wojtek Mach <wojtek@wojtekmach.pl>
2021-05-31 17:17:14 +0200

    Add benchee.exs

---
+++ benchee.exs
@@ -0,0 +1,15 @@
+Mix.install([
+  {:benchee, "~> 1.0"}
+])
+
+list = Enum.to_list(1..10_000)
+map_fun = fn i -> [i, i * i] end
+
+Benchee.run(
+  %{
+    "flat_map" => fn -> Enum.flat_map(list, map_fun) end,
+    "map.flatten" => fn -> list |> Enum.map(map_fun) |> List.flatten() end
+  },
+  time: 10,
+  memory_time: 2
+)
