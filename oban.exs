a19021140cd46c4c5e5db75eb03187d7460be361
Wojtek Mach <wojtek@wojtekmach.pl>
2021-08-24 11:06:30 +0200

    Add oban.exs

---
+++ oban.exs
@@ -0,0 +1,54 @@
+Mix.install([
+  {:ecto_sql, "~> 3.6.2"},
+  {:postgrex, "~> 0.15.0"},
+  {:oban, "~> 2.8"}
+])
+
+Application.put_env(:myapp, Repo, database: "mix_install_oban")
+
+defmodule Repo do
+  use Ecto.Repo,
+    adapter: Ecto.Adapters.Postgres,
+    otp_app: :myapp
+end
+
+defmodule Migration0 do
+  use Ecto.Migration
+
+  def change do
+    Oban.Migrations.up()
+  end
+end
+
+defmodule Main do
+  def main do
+    children = [
+      Repo,
+      {Oban, repo: Repo, plugins: [Oban.Plugins.Pruner], queues: [default: 10]}
+    ]
+
+    Repo.__adapter__().storage_down(Repo.config())
+    Repo.__adapter__().storage_up(Repo.config())
+    {:ok, _} = Supervisor.start_link(children, strategy: :one_for_one)
+
+    Ecto.Migrator.run(Repo, [{0, Migration0}], :up, all: true)
+
+    Oban.insert!(Worker.new(%{id: 1}))
+    Oban.insert!(Worker.new(%{id: 2}))
+
+    Oban.Job
+    |> Repo.all()
+    |> IO.inspect()
+  end
+end
+
+defmodule Worker do
+  use Oban.Worker
+
+  @impl true
+  def perform(%Oban.Job{}) do
+    :ok
+  end
+end
+
+Main.main()
