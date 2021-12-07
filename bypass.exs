02c476737457e35da459d2beaf6442bb64611c15
Wojtek Mach <wojtek@wojtekmach.pl>
2021-12-07 06:15:06 +0100

    Update bypass.exs

--- bypass.exs
+++ bypass.exs
@@ -12,10 +12,6 @@ defmodule BypassTest do
     bypass = Bypass.open()
 
     Bypass.expect(bypass, "GET", "/", fn conn ->
-      assert Plug.Conn.get_req_header(conn, "authorization") == [
-               "Basic #{:base64.encode("john:doe")}"
-             ]
-
       Plug.Conn.send_resp(conn, 200, "ok")
     end)
 
