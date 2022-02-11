d6b8303dda178be4577231209cd51415766e254a
Tonći Galić <tuxified@gmail.com>
2022-02-11 14:28:00 +0100

    Add sourceror.exs (#6)

---
+++ sourceror.exs
@@ -0,0 +1,32 @@
+# Example of Code Mod
+#
+# Changes:
+#
+#     |> (fn ... end().()
+#
+# into:
+#
+#     |> then(fn ... end)
+
+Mix.install([
+  {:sourceror, "~> 0.9"}
+])
+
+source = """
+42
+|> (fn i -> i * 2 end).()
+"""
+
+IO.puts(["Before: \n", source, "\nAfter: "])
+
+source
+|> Sourceror.parse_string!()
+|> Sourceror.postwalk(fn
+  {:|>, meta, [prev, {{:., _, [{:fn, _, _} | _] = contents}, _, _}]}, state ->
+    {{:|>, meta, [prev, {:then, [], contents}]}, state}
+
+  rest, state ->
+    {rest, state}
+end)
+|> Sourceror.to_string()
+|> IO.puts()
