83bb4b0d0b616a23cfb3534a24699a181fbcbeec
Wojtek Mach <wojtek@wojtekmach.pl>
2022-02-18 22:06:26 +0100

    Add bandit.exs

---
+++ bandit.exs
@@ -0,0 +1,28 @@
+Mix.install([
+  {:bandit, ">= 0.0.0"}
+])
+
+defmodule Router do
+  use Plug.Router
+  plug(Plug.Logger)
+  plug(:match)
+  plug(:dispatch)
+
+  get "/" do
+    send_resp(conn, 200, "Hello, World!")
+  end
+
+  match _ do
+    send_resp(conn, 404, "not found")
+  end
+end
+
+bandit = {Bandit, plug: Router, scheme: :http, port: 4000}
+require Logger
+Logger.info("starting #{inspect(bandit)}")
+{:ok, _} = Supervisor.start_link([bandit], strategy: :one_for_one)
+
+# unless running from IEx, sleep idenfinitely so we can serve requests
+unless IEx.started?() do
+  Process.sleep(:infinity)
+end
