271d2d8e3b8c893e16dac634d418339af04df42d
Wojtek Mach <wojtek@wojtekmach.pl>
2022-02-21 17:25:14 +0100

    Run formatter

--- gen_stage.exs
+++ gen_stage.exs
@@ -16,7 +16,7 @@ defmodule Producer do
   def handle_demand(demand, counter) do
     # If the counter is 3 and we ask for 2 items, we will
     # emit the items 3 and 4, and set the state to 5.
-    events = Enum.to_list(counter..counter+demand-1)
+    events = Enum.to_list(counter..(counter + demand - 1))
     {:noreply, events, counter + demand}
   end
 end
