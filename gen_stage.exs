13190a8f6c264544f000ada518b5ab4e00025029
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2022-02-21 17:23:22 +0100

    Add comments and output

--- gen_stage.exs
+++ gen_stage.exs
@@ -9,12 +9,15 @@ defmodule Producer do
     GenStage.start_link(__MODULE__, [], name: Producer)
   end
 
-  def init(initial_state) do
-    {:producer, initial_state}
+  def init(_) do
+    {:producer, 0}
   end
 
-  def handle_demand(_demand, state) do
-    {:noreply, [], state}
+  def handle_demand(demand, counter) do
+    # If the counter is 3 and we ask for 2 items, we will
+    # emit the items 3 and 4, and set the state to 5.
+    events = Enum.to_list(counter..counter+demand-1)
+    {:noreply, events, counter + demand}
   end
 end
 
@@ -25,8 +28,19 @@ defmodule Consumer do
     GenStage.start_link(__MODULE__, [])
   end
 
-  def init(initial_state) do
-    {:consumer, initial_state, subscribe_to: [Producer]}
+  def init(_) do
+    {:consumer, :whatever, subscribe_to: [{Producer, min_demand: 0, max_demand: 5}]}
+  end
+
+  def handle_events(events, _from, state) do
+    # Wait for a second.
+    Process.sleep(1_000)
+
+    # Inspect the events.
+    IO.inspect(events)
+
+    # We are a consumer, so we would never emit items.
+    {:noreply, [], state}
   end
 end
 
