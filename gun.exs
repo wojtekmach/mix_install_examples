0cbef87ed2c436fe0b6f8366bbf625e98b7c7c3b
Wojtek Mach <wojtek@wojtekmach.pl>
2022-05-06 11:20:16 +0200

    Add gun.exs

---
+++ gun.exs
@@ -0,0 +1,10 @@
+Mix.install([
+  {:gun, "1.3.3"}
+])
+
+{:ok, conn_pid} = :gun.open('httpbin.org', 443)
+{:ok, :http2} = :gun.await_up(conn_pid)
+stream_ref = :gun.post(conn_pid, "/anything", [{"user-agent", "gun/1.3.3"}], "hi")
+{:response, :nofin, status, headers} = :gun.await(conn_pid, stream_ref)
+{:ok, body} = :gun.await_body(conn_pid, stream_ref)
+IO.inspect(status: status, headers: headers, body: body)
