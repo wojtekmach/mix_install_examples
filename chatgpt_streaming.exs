7241c1210fe1675aaa48cf218f80ca1d8e7ff56b
Neil Berkman <neil@berkman.com>
2023-03-11 14:04:50 -0800

    Streaming ChatGPT example

---
+++ chatgpt_streaming.exs
@@ -0,0 +1,90 @@
+# Usage: elixir chatgpt_streaming.exs openai-api-key "prompt"
+
+Mix.install([
+  {:eventsource_ex, "~> 2.0"},
+  {:httpoison, "~> 1.5"},
+  {:jason, "~> 1.4"}
+])
+
+defmodule ChatGPTStreamer do
+  use GenServer
+
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state)
+  end
+
+  def init(state) do
+    {:ok, request_completion(state[:api_key], state[:messages], state[:parent_pid])}
+  end
+
+  def handle_info(
+        %EventsourceEx.Message{
+          data: "[DONE]",
+          event: "message"
+        },
+        parent_pid
+      ) do
+    IO.puts("")
+    send(parent_pid, :done)
+    {:stop, :normal, nil}
+  end
+
+  def handle_info(
+        %EventsourceEx.Message{
+          data: jason_payload,
+          event: "message"
+        },
+        state
+      ) do
+    message = Jason.decode!(jason_payload)
+    extract_text(message) |> IO.write()
+    {:noreply, state}
+  end
+
+  def extract_text(%{"choices" => choices}) do
+    choices
+    |> Enum.map(fn %{"delta" => delta} -> delta end)
+    |> Enum.filter(fn content -> Map.has_key?(content, "content") end)
+    |> Enum.map(fn %{"content" => content} -> content end)
+    |> Enum.join()
+  end
+
+  def extract_text(_) do
+    ""
+  end
+
+  @chat_completion_url "https://api.openai.com/v1/chat/completions"
+  @model "gpt-3.5-turbo"
+
+  defp request_completion(api_key, messages, parent_pid) do
+    body = %{
+      model: @model,
+      messages: messages,
+      stream: true
+    }
+
+    headers = [
+      {"Authorization", "Bearer #{api_key}"},
+      {"Content-Type", "application/json"}
+    ]
+
+    options = [method: :post, body: Jason.encode!(body), headers: headers]
+    {:ok, _pid} = EventsourceEx.new(@chat_completion_url, options)
+    parent_pid
+  end
+end
+
+Logger.configure(level: :info)
+
+[api_key, prompt] = System.argv()
+
+{:ok, _pid} =
+  ChatGPTStreamer.start_link(
+    api_key: api_key,
+    messages: [%{role: "user", content: prompt}],
+    parent_pid: self()
+  )
+
+receive do
+  :done -> true
+end
