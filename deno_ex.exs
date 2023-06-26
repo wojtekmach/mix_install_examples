1a42638595a2ef9d0f127061c4bd9484b0ac9268
Wojtek Mach <wojtekmach@users.noreply.github.com>
2023-06-26 09:27:21 +0200

    Update deno_ex.exs

--- deno_ex.exs
+++ deno_ex.exs
@@ -1,11 +1,4 @@
 Mix.install([:deno_ex])
 
-ExUnit.start()
-
-defmodule DenoExTest do
-  use ExUnit.Case, async: true
-
-  test "deno_ex" do
-    assert {:ok, "Hello, world.\n"} == DenoEx.run({:stdin, "console.log(\"Hello, world.\")"})
-  end
-end
+DenoEx.run({:stdin, ~s|console.log("Hello, world.")|})
+|> IO.inspect()
