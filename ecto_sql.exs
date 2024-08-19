1c72091b603073470b4f8c0e3b0f59379cf3dc53
Wojtek Mach <wojtek@wojtekmach.pl>
2024-08-19 14:27:41 +0200

    Update ecto_sql.exs

--- ecto_sql.exs
+++ ecto_sql.exs
@@ -6,9 +6,7 @@ Mix.install([
 Application.put_env(:foo, Repo, database: "mix_install_examples")
 
 defmodule Repo do
-  use Ecto.Repo,
-    adapter: Ecto.Adapters.Postgres,
-    otp_app: :foo
+  use Ecto.Repo, adapter: Ecto.Adapters.Postgres, otp_app: :foo
 end
 
 defmodule Migration0 do
@@ -19,11 +17,6 @@ defmodule Migration0 do
       add(:title, :string)
       timestamps(type: :utc_datetime_usec)
     end
-
-    create table("comments") do
-      add(:content, :string)
-      add(:post_id, references(:posts, on_delete: :delete_all), null: false)
-    end
   end
 end
 
@@ -33,16 +26,6 @@ defmodule Post do
   schema "posts" do
     field(:title, :string)
     timestamps(type: :utc_datetime_usec)
-    has_many(:comments, Comment)
-  end
-end
-
-defmodule Comment do
-  use Ecto.Schema
-
-  schema "comments" do
-    field(:content, :string)
-    belongs_to(:post, Post)
   end
 end
 
@@ -50,27 +33,17 @@ defmodule Main do
   import Ecto.Query, warn: false
 
   def main do
-    children = [
-      Repo
-    ]
-
-    _ = Repo.__adapter__().storage_down(Repo.config())
+    Repo.__adapter__().storage_down(Repo.config())
     :ok = Repo.__adapter__().storage_up(Repo.config())
-
-    {:ok, _} = Supervisor.start_link(children, strategy: :one_for_one)
-
-    Ecto.Migrator.run(Repo, [{0, Migration0}], :up, all: true, log_migrations_sql: :debug)
+    {:ok, _} = Supervisor.start_link([Repo], strategy: :one_for_one)
+    Ecto.Migrator.run(Repo, [{0, Migration0}], :up, all: true, log_migrations_sql: :info)
 
     Repo.insert!(%Post{
-      title: "Post 1",
-      comments: [%Comment{content: "Comment 1"}, %Comment{content: "Comment 2"}]
+      title: "Post 1"
     })
 
-    Repo.insert!(%Post{title: "Post 2", comments: [%Comment{content: "Comment 3"}]})
-
-    from(p in Post, join: c in assoc(p, :comments))
-    |> Repo.all()
-    |> IO.inspect()
+    Repo.all(from(p in Post))
+    |> dbg()
   end
 end
 
