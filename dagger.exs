4692e0a10f7ed3c3f4fef3e46853b3468603d71b
Wojtek Mach <wojtekmach@users.noreply.github.com>
2024-09-06 12:07:36 +0200

    Update dagger.exs

--- dagger.exs
+++ dagger.exs
@@ -9,15 +9,16 @@ Dagger.with_connection(
       |> Dagger.GitRepository.branch("main")
       |> Dagger.GitRef.tree()
 
-    {:ok, version} = dag
-    |> Dagger.Client.container()
-    |> Dagger.Container.from("hexpm/erlang:27.0.1-alpine-3.20.2")
-    |> Dagger.Container.with_exec(~w"apk add make")
-    |> Dagger.Container.with_workdir("/elixir")
-    |> Dagger.Container.with_mounted_directory("/elixir", source)
-    |> Dagger.Container.with_exec(~w"make")
-    |> Dagger.Container.with_exec(~w"./bin/elixir --version")
-    |> Dagger.Container.stdout()
+    {:ok, version} =
+      dag
+      |> Dagger.Client.container()
+      |> Dagger.Container.from("hexpm/erlang:27.0.1-alpine-3.20.2")
+      |> Dagger.Container.with_exec(~w"apk add make")
+      |> Dagger.Container.with_workdir("/elixir")
+      |> Dagger.Container.with_mounted_directory("/elixir", source)
+      |> Dagger.Container.with_exec(~w"make")
+      |> Dagger.Container.with_exec(~w"./bin/elixir --version")
+      |> Dagger.Container.stdout()
 
     IO.puts(version)
   end,
